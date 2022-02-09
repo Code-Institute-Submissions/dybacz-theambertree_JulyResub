ffa01c22acbae87e0f65f35fc1c850eaa5b3263e
dybacz <jdybacz@gmail.com>
2022-02-09 03:00:04 +0000

    +Improved Admin Panel Features, updated models

--- bookingsys/admin.py
+++ bookingsys/admin.py
@@ -1,7 +1,46 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.Table)
-admin.site.register(models.BookingSlot)
-admin.site.register(models.Booking)
-admin.site.register(models.TimeSlot)
+
+@admin.register(models.Table)
+class TableAdmin(admin.ModelAdmin):
+    list_display = ('table_name', 'table_capacity')
+    search_fields = ['table_name', 'table_capacity']
+    list_filter = ('table_name', 'table_capacity')
+
+
+@admin.register(models.BookingSlot)
+class BookingSlotAdmin(admin.ModelAdmin):
+    list_display = (
+        'table_number',
+        'table_capacity',
+        'date',
+        'time_slot',
+        'status',
+        'booking_status')
+    search_fields = ['table', 'date', 'time_slot', 'status', 'booking_status']
+    list_filter = ('date', 'status', 'booking_status')
+
+
+@admin.register(models.Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = (
+        'booking_id',
+        'guest',
+        'booking_table',
+        'number_of_guests',
+        'total_capacity',
+        'reservation_date',
+        'reservation_time',
+        'status',
+        'date_of_booking',
+        'comments')
+    search_fields = ()
+    list_filter = ('status',)
+
+
+@admin.register(models.TimeSlot)
+class TimeSlotAdmin(admin.ModelAdmin):
+    list_display = ('startTime', 'endTime', 'closed')
+    search_fields = ['startTime']
+    list_filter = ('closed',)
--- bookingsys/models.py
+++ bookingsys/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib import admin
+
 
 A_STATUS = ((0, "Draft"), (1, "Published"))
 B_STATUS = ((0, "Booked"), (1, "Availble"))
-C_STATUS = ((0, "Booked"), (1, "Cancelled"))
+C_STATUS = ((0, "Booked"), (1, "Cancelled"), (2, "Redeemed"))
 D_STATUS = ((0, "Closed"), (1, "Open"))
 
 
@@ -17,13 +19,13 @@ class Table(models.Model):
         ordering = ['-table_name']
 
     def __str__(self):
-        return f"| Table: {self.table_name} | Capacity: {self.table_capacity}"
+        return f"Table: {self.table_name} | Capacity: {self.table_capacity}"
 
 
 class TimeSlot(models.Model):
     ''' Model for Timeslots '''
-    startTime = models.TimeField(unique=True)
-    endTime = models.TimeField(unique=True)
+    startTime = models.TimeField()
+    endTime = models.TimeField()
     closed = models.IntegerField(choices=D_STATUS, default=0)
 
     class Meta:
@@ -31,7 +33,7 @@ class TimeSlot(models.Model):
         ordering = ['-startTime']
 
     def __str__(self):
-        return f"Reservation: {self.startTime} - {self.endTime}"
+        return f"{self.startTime} - {self.endTime}"
 
 
 class BookingSlot (models.Model):
@@ -43,9 +45,11 @@ class BookingSlot (models.Model):
         blank=True,
         null=True)
     date = models.DateField(blank=True, null=True)
-    timeSlot = models.ForeignKey(
+    time_slot = models.ForeignKey(
         TimeSlot,
         on_delete=models.CASCADE,
+        limit_choices_to={
+            'closed': 1},
         related_name="bookingslot",
         blank=True,
         null=True)
@@ -57,7 +61,23 @@ class BookingSlot (models.Model):
         ordering = ['-date']
 
     def __str__(self):
-        return f"{self.date} {self.timeSlot} {self.table}"
+        return f"{self.table} | {self.date} | {self.time_slot}"
+
+    @admin.display
+    def table_capacity(self):
+        ''' Loop for displaying table capcity on admin booking slot panel '''
+        capacity = 0
+        # for i in self.time_slot.all():
+        x_string = str(self.table).split("| ")
+        z_string = x_string[1].split(" ")
+        capacity += int(z_string[1])
+        #     concat += x_list[1]
+        return capacity
+
+    def table_number(self):
+        ''' Loop for displaying table number on admin booking slot panel '''
+        x_string = str(self.table).split(" ")
+        return x_string[1]
 
 
 class Booking(models.Model):
@@ -70,7 +90,8 @@ class Booking(models.Model):
         blank=True,
         null=True)
     update_on = models.DateTimeField(auto_now=True)
-    created_on = models.DateTimeField(auto_now_add=True)
+    number_of_guests = models.IntegerField()
+    date_of_booking = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=C_STATUS, default=0)
     comments = models.CharField(max_length=200)
     timeslot = models.ManyToManyField(
@@ -83,7 +104,41 @@ class Booking(models.Model):
 
     class Meta:
         ''' Method for ordering data '''
-        ordering = ['-created_on']
+        ordering = ['-date_of_booking']
 
     def __str__(self):
         return f"{self.booking_id}"
+
+    @admin.display
+    def booking_table(self):
+        ''' Loop for displaying table number on admin booking panel '''
+        concat = []
+        for i in self.timeslot.get_queryset():
+            x_list = str(i).split("| ")
+            concat.append(x_list[0])
+        concat.reverse()
+        return concat
+
+    def total_capacity(self):
+        '''Loop for displaying booking total capcity on admin booking panel
+        '''
+        concat = ""
+        capacity = 0
+        for i in self.timeslot.get_queryset():
+            x_list = str(i).split("| ")
+            z_list = x_list[1].split(" ")
+            capacity += int(z_list[1])
+            concat += x_list[1]
+        return f"{capacity}"
+
+    def reservation_date(self):
+        ''' Loop for displaying reservation date on admin booking panel '''
+        for i in self.timeslot.get_queryset():
+            x_list = str(i).split("| ")
+            return f"{x_list[2]}"
+
+    def reservation_time(self):
+        ''' Loop for displaying reservation time on admin booking panel '''
+        for i in self.timeslot.get_queryset():
+            x_list = str(i).split("| ")
+            return f"{x_list[3]}"
