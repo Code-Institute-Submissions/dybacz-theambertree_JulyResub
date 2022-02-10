169f6e6bd99f1f3a9f35c480f467b6440ecf8fe0
dybacz <jdybacz@gmail.com>
2022-02-10 18:30:12 +0000

    allauth install, bootstrap 5, views & urls updated

--- bookingsys/views.py
+++ bookingsys/views.py
@@ -6,5 +6,5 @@ from . import models
 class BookingSlotList(generic.ListView):
     model = models.BookingSlot
     queryset = models.BookingSlot.objects.filter(status=1).order_by('-date')
-    template_name = 'index.html'
+    template_name = 'bookings.html'
     paginate_by = 6
--- theambertree/urls.py
+++ theambertree/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('bookings/', include('bookingsys.urls'), name="bookingsys_urls"),
+    path('', views.index, name='index'),
+    path('bookings/', include('bookingsys.urls'), name="bookingsys"),
+    path('accounts/', include('allauth.urls')),
 ]
