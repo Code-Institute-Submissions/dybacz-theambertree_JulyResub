12c81c8bf258c191a085ff95c46395a177554e75
dybacz <80072323+dybacz@users.noreply.github.com>
2022-02-15 07:57:47 +0000

    Font awesome Icons added, buttons cleaned up

--- static/js/script.js
+++ static/js/script.js
@@ -23,20 +23,27 @@ $(document).ready(function() {
         if (x < windowHeight/2) {
             $("#about-link").addClass("active")
             $("#menu-link").removeClass("active")
+            $("#drinks-link").removeClass("active")
+            $("#contact-link").removeClass("active")
         }
         if (x > windowHeight/2 && x < (3/2)*windowHeight) {
+            $("#menu-link").addClass("active")
             $("#about-link").removeClass("active")
             $("#drinks-link").removeClass("active")
-            $("#menu-link").addClass("active")
+            $("#contact-link").removeClass("active")
+
         }
         if (x > (3/2)*windowHeight && x < (5/2)*windowHeight){
+            $("#drinks-link").addClass("active")
+            $("#about-link").removeClass("active")
             $("#menu-link").removeClass("active")
             $("#contact-link").removeClass("active")
-            $("#drinks-link").addClass("active")  
         }
         if (x >= (5/2)*windowHeight){
-            $("#drinks-link").removeClass("active")
             $("#contact-link").addClass("active")
+            $("#about-link").removeClass("active")
+            $("#menu-link").removeClass("active")
+            $("#drinks-link").removeClass("active")
         }
     }
    
