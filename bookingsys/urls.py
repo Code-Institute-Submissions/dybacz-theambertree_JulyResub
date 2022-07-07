af10f2a60d472810ebd0ebbc160120cdc3297549
dybacz <80072323+dybacz@users.noreply.github.com>
2022-07-08 00:23:36 +0100

    +booking remove url path

--- bookingsys/urls.py
+++ bookingsys/urls.py
@@ -5,5 +5,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.BookingSlot.as_view(), name='bookings_home'),
     path('ajax/<data_request>/', views.AjaxRequest.as_view(), name='ajax_request'),
-    path('my-bookings', views.Bookings.as_view(), name='my_bookings')
+    path('my-bookings', views.Bookings.as_view(), name='my_bookings'),
+    path('my-bookings/remove/<booking_id>', views.Bookings.as_view(), name='my_bookings_remove')
 ]
