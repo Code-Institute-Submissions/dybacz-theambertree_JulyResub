64a8e17eccbea1c5b95214f82e39338399db4462
dybacz <80072323+dybacz@users.noreply.github.com>
2022-07-10 06:10:22 +0100

    comma added to end of urlpatterns

--- bookingsys/urls.py
+++ bookingsys/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.BookingSlot.as_view(), name='bookings_home'),
     path('ajax/<data_request>/', views.AjaxRequest.as_view(), name='ajax_request'),
     path('my-bookings', views.Bookings.as_view(), name='my_bookings'),
-    path('my-bookings/remove/<booking_id>', views.Bookings.as_view(), name='my_bookings_remove')
+    path('my-bookings/remove/<booking_id>', views.Bookings.as_view(), name='my_bookings_remove'),
 ]
