188955b2765be0ea15e09c1177b7f697229ce4a6
dybacz <80072323+dybacz@users.noreply.github.com>
2022-07-11 02:58:57 +0100

    +schedule remove and add urls

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -13,6 +13,8 @@ urlpatterns = [
     path('times/add/', views.DashboardTimes.as_view()),
     path('times/edit/', views.DashboardTimes.as_view()),
     path('schedule/', views.DashboardSchedule.as_view(), name='dashboard_schedule'),
+    path('schedule/remove/<bookingslot_id>', views.DashboardSchedule.as_view()),
+    path('schedule/add/', views.DashboardSchedule.as_view()),
     path('bookings/', views.DashboardBookings.as_view(), name='dashboard_bookings'),
     path('menu/', views.DashboardBookings.as_view(), name='dashboard_menu'),
     path('messages/', views.DashboardMessages.as_view(), name='dashboard_messages'),
