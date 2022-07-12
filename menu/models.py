661d9b1336bd4c0c25fbabb0d8b38fa20cc8e0ac
dybacz <80072323+dybacz@users.noreply.github.com>
2022-07-13 00:54:38 +0100

    max digits change and null and blank = true

--- menu/models.py
+++ menu/models.py
@@ -37,7 +37,8 @@ class Item(models.Model):
         'Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
     description = models.TextField(null=True, blank=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(
+        max_digits=8, decimal_places=2, null=True, blank=True)
     rating = models.DecimalField(
         max_digits=4, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
